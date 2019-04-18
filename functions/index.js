6cdbf04af2166dc3edec24badab002eedbe8c526
Samuel Lippert <SLippert@zebra.com>
2019-04-18 16:17:57 -0500

    Add Visual Studio solution and convert to firebase http function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const bwipjs = require('./gcp-bwipjs');	// ./ required for local use
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.bwipjs = functions.https.onRequest((req, res) => {
+    // If the url does not begin /?bcid= then 404.  Otherwise, we end up
+    // returning 400 on requests like favicon.ico.
+    if (req.url.indexOf('/?bcid=') != 0) {
+        res.status(404)
+           .contentType('text/plain')
+           .send('BWIP-JS: Unknown request format.');
+    } else {
+        // Do not allow images to grow too large
+        bwipjs(req, res, { sizelimit:2048*2048 });
+    }
+});
