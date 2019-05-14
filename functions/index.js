8125f9604ed809db33b5325cad9f9686c3009d8d
Samuel Lippert <SLippert@zebra.com>
2019-05-14 16:08:03 -0500

    Add authorization header requirement

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,12 @@ const functions = require('firebase-functions');
 const bwipjs = require('./gcp-bwipjs');	// ./ required for local use
 
 exports.bwipjs = functions.https.onRequest((req, res) => {
+    let secret = req.headers.authorization
+    // TODO: Grab key from secure store
+    if (!(secret === "SavannaIsCool!987")) {
+        res.status(401).send({ message: "Authorization failure" })
+        return
+    }
     // Do not allow images to grow too large
     bwipjs(req, res, { sizelimit:2048*2048 });
 });
